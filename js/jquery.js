071bb384ab8b5cb83964daffa27fef7a8d78a074
Alif-Mahamud <117834835+Alif-Mahamud@users.noreply.github.com>
2023-01-14 23:51:02 +0600

    Add files via upload

---
+++ js/jquery.js
@@ -0,0 +1,11 @@
+$('.slider_part').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 700,
+  arrows:true,
+  prevArrow:' <i class="fa-solid fa-chevron-left prev"></i>',
+  nextArrow:'<i class="fa-solid fa-chevron-right next"></i>',
+  dots:false,
+});
+
